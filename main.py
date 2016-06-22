0118db3253d1ce9dedf743bec4bf4853ee09fe62
norang <pinethee@naver.com>
2016-06-23 06:53:13 +0900

    메인 뼈대 추가

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from Node import *
-
 RUNNING_TIME = 1000000000   # 1000초
 NODES = 2                   # node의 개수
 
 node = []                   # 현재 line에 연결된 node 객체들
 transmitting = []           # 현재 line에 data를 전송중인 node의 번호
+
+collision = 0               # 총 packet 충돌 횟수
+success = 0                 # 총 packet 전송 성공 횟수
+delay = 0                   # 총 delay 시간
 
 for num in range(1, NODES):
     node.append(Node(1))
@@ -19,6 +21,12 @@ for time in range(1, RUNNING_TIME):
         # 한번에 여러 노드가 전송중인 경우 충돌이 난 것
         for n in transmitting:
             node[n].collide()
+            collision += 1
+
+
+# Throughput = (sum of success)/(total time)
+# Mean packet delay = (Sum of delays)/(Total success)
+# Transmission collision probability = (Num. of collision)/(Total trial)
 
 
 
