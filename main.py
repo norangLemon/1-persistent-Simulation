43413889093a05432bc7cf7b9dc78d29bde407c1
norang <pinethee@naver.com>
2016-06-23 02:41:50 +0900

    2개 노드를 시범적으로 돌릴 수 있는 메인 뼈대 코드

---
+++ main.py
@@ -0,0 +1,24 @@
+from Node import *
+
+RUNNING_TIME = 1000000000   # 1000초
+NODES = 2                   # node의 개수
+
+node = []                   # 현재 line에 연결된 node 객체들
+transmitting = []           # 현재 line에 data를 전송중인 node의 번호
+
+for num in range(1, NODES):
+    node.append(Node(1))
+
+for time in range(1, RUNNING_TIME):
+
+    for n in range(1, NODES):
+        # 이번 시간에 각 노드에서 수행하는 일을 진행시킴
+        node[n].process()
+
+    if len(transmitting) > 1:
+        # 한번에 여러 노드가 전송중인 경우 충돌이 난 것
+        for n in transmitting:
+            node[n].collide()
+
+
+
